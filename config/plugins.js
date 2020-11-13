bdaac825b28a1dee03548bfbcb17440b8693464d
Ryan Krueger <rkrueger11@gmail.com>
2020-11-13 10:58:21 -0500

    buncho changes

---
+++ config/plugins.js
@@ -0,0 +1,12 @@
+module.exports = ({ env }) => ({
+    // ...
+    upload: {
+      provider: 'cloudinary',
+      providerOptions: {
+        cloud_name: 'ryans-leukemia-journey',
+        api_key: '436952556186331',
+        api_secret: 'o1Mr-LORnlYtBUJQJfaEd86nRYs',
+      },
+    },
+    // ...
+  });
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'aa5eb7c5-6c9f-40ef-8825-2d286b4f0faa'
+};
