e26c2d1d2d473b831724bf5c43333693c9b67f5c
Ryan Krueger <rkrueger11@gmail.com>
2020-11-13 00:38:51 -0500

    added cloudinary

--- config/database.js
+++ config/database.js
@@ -6,10 +6,10 @@ module.exports = ({ env }) => ({
       settings: {
         client: 'postgres',
         host: env('DATABASE_HOST', '127.0.0.1'),
-        port: env.int('DATABASE_PORT', 27017),
+        port: env.int('DATABASE_PORT', 5432),
         database: env('DATABASE_NAME', 'postgres'),
-        username: env('DATABASE_USERNAME', ''),
-        password: env('DATABASE_PASSWORD', ''),
+        username: env('DATABASE_USERNAME', 'postgres'),
+        password: env('DATABASE_PASSWORD', 'pass'),
       },
       options: {
         ssl: false,
