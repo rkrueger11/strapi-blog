47e3a01ee6a6258c040f59cad48f36762f1e91dc
Ryan Krueger <rkrueger11@gmail.com>
2020-11-15 13:31:24 -0500

    followed https://talke.dev/strapi-user-permissions-jwt-cookies tutorial to move jwt to a httpOnly cookie rather than storing it in the front end

--- config/plugins.js
+++ config/plugins.js
@@ -7,6 +7,19 @@ module.exports = ({ env }) => ({
         api_key: '436952556186331',
         api_secret: 'o1Mr-LORnlYtBUJQJfaEd86nRYs',
       },
+  },
+  email: {
+    provider: "mailjet",
+    providerOptions: {
+      publicApiKey: env("MAILJET_PUBLIC_KEY"),
+      secretApiKey: env("MAILJET_SECRET_KEY"),
     },
+    settings: {
+      defaultFrom: "alerts@ryansjourney.blog",
+      defaultFromName: "Ryan Krueger",
+      defaultTo: "rkrueger11@gmail.com",
+      defaultToName: "Ryan Krueger (test)",
+    },
+  },
     // ...
-  });
+});
